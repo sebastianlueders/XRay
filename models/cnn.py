8ec13d7ca8aae35273bc2fcb02b1c3b7b771f820
sidxcviii <siddharthdutta85@gmail.com>
2025-04-22 19:29:13 -0500

    Buit CNN model

---
+++ models/cnn.py
@@ -0,0 +1,90 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class DeepCNN(nn.Module):
+
+    def __init__(self, num_classes=8):
+        super(DeepCNN, self).__init__()
+        # first block
+        self.conv1_1 = nn.Conv2d(1, 64, kernel_size=3, padding=1)
+        self.bn1_1 = nn.BatchNorm2d(64)
+        self.conv1_2 = nn.Conv2d(64, 64, kernel_size=3, padding=1)
+        self.bn1_2 = nn.BatchNorm2d(64)
+        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.dropout1 = nn.Dropout(0.25)
+        
+        # second block
+        self.conv2_1 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
+        self.bn2_1 = nn.BatchNorm2d(128)
+        self.conv2_2 = nn.Conv2d(128, 128, kernel_size=3, padding=1)
+        self.bn2_2 = nn.BatchNorm2d(128)
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.dropout2 = nn.Dropout(0.25)
+        
+        # third block
+        self.conv3_1 = nn.Conv2d(128, 256, kernel_size=3, padding=1)
+        self.bn3_1 = nn.BatchNorm2d(256)
+        self.conv3_2 = nn.Conv2d(256, 256, kernel_size=3, padding=1)
+        self.bn3_2 = nn.BatchNorm2d(256)
+        self.conv3_3 = nn.Conv2d(256, 256, kernel_size=3, padding=1)
+        self.bn3_3 = nn.BatchNorm2d(256)
+        self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.dropout3 = nn.Dropout(0.25)
+        
+        # fourth block
+        self.conv4_1 = nn.Conv2d(256, 512, kernel_size=3, padding=1)
+        self.bn4_1 = nn.BatchNorm2d(512)
+        self.conv4_2 = nn.Conv2d(512, 512, kernel_size=3, padding=1)
+        self.bn4_2 = nn.BatchNorm2d(512)
+        self.conv4_3 = nn.Conv2d(512, 512, kernel_size=3, padding=1)
+        self.bn4_3 = nn.BatchNorm2d(512)
+        self.pool4 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.dropout4 = nn.Dropout(0.25)
+        
+        # FC layers
+        self.flatten = nn.Flatten()
+        self.fc1 = nn.Linear(512 * 3 * 3, 1024)
+        self.bn5 = nn.BatchNorm1d(1024)
+        self.dropout5 = nn.Dropout(0.5)
+        self.fc2 = nn.Linear(1024, 512)
+        self.bn6 = nn.BatchNorm1d(512)
+        self.dropout6 = nn.Dropout(0.5)
+        self.fc3 = nn.Linear(512, num_classes)
+    
+    def forward(self, x):
+        # first block
+        x = F.relu(self.bn1_1(self.conv1_1(x)))
+        x = F.relu(self.bn1_2(self.conv1_2(x)))
+        x = self.pool1(x)
+        x = self.dropout1(x)
+        
+        # second block
+        x = F.relu(self.bn2_1(self.conv2_1(x)))
+        x = F.relu(self.bn2_2(self.conv2_2(x)))
+        x = self.pool2(x)
+        x = self.dropout2(x)
+        
+        # third block
+        x = F.relu(self.bn3_1(self.conv3_1(x)))
+        x = F.relu(self.bn3_2(self.conv3_2(x)))
+        x = F.relu(self.bn3_3(self.conv3_3(x)))
+        x = self.pool3(x)
+        x = self.dropout3(x)
+        
+        # fourth block
+        x = F.relu(self.bn4_1(self.conv4_1(x)))
+        x = F.relu(self.bn4_2(self.conv4_2(x)))
+        x = F.relu(self.bn4_3(self.conv4_3(x)))
+        x = self.pool4(x)
+        x = self.dropout4(x)
+        
+        # FC layers
+        x = self.flatten(x)
+        x = F.relu(self.bn5(self.fc1(x)))
+        x = self.dropout5(x)
+        x = F.relu(self.bn6(self.fc2(x)))
+        x = self.dropout6(x)
+        x = self.fc3(x)
+        
+        return x
