041b47fa6d8d87405ba9181ab73d72ebbf3e82dd
Seb Lueders <38769233+sebastianlueders@users.noreply.github.com>
2025-04-27 14:30:12 -0500

    Added dataset for training/testing

---
+++ models/ResNet.py
@@ -0,0 +1,11 @@
+import torch
+import pandas as pd
+import numpy as np
+from tqdm import tqdm
+
+from torch.utils.data import DataLoader
+from torchvision import transforms
+import torch.optim as optim
+import matplotlib.pyplot as plt
+
+
